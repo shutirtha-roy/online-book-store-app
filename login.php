7ee05dc6233e79517590d7e7ed6daca7d4cc046a
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-04-01 00:46:28 +0600

    Separate navigation location added

--- login.php
+++ login.php
@@ -56,33 +56,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 <body class="bg-dark text-light">
 
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        
-
-
-
-      
-        <div class="collapse navbar-collapse container" id="navbarSupportedContent">
-          <a class="navbar-brand" href="#">Roy</a>
-            
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="books.html">Books</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="register.html">Register</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="login.php">Login</a>
-            </li>
- 
-
-          </ul>
-        </div>
-    </nav>
+    <?php require 'nav.php'; ?>
 
 
    
---
+++ nav.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        
+
+
+
+      
+        <div class="collapse navbar-collapse container" id="navbarSupportedContent">
+          <a class="navbar-brand" href="#">Roy</a>
+            
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="books.html">Books</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="register.html">Register</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="login.php">Login</a>
+            </li>
+ 
+
+          </ul>
+        </div>
+    </nav>
