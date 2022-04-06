b3db287d23e7bf38a3a3b0a8059b5caf1b6970b9
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-04-07 02:53:35 +0600

    Tables Updated For Cart

--- book_store_database.sql
+++ book_store_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2022 at 11:19 PM
+-- Generation Time: Apr 06, 2022 at 10:51 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.14
 
@@ -54,6 +54,32 @@ INSERT INTO `books` (`id`, `category_id`, `name`, `author`, `description`, `pric
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `book_cart`
+--
+
+CREATE TABLE `book_cart` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book_cart`
+--
+
+INSERT INTO `book_cart` (`id`, `user_id`, `product_id`, `user_name`, `product_name`, `price`) VALUES
+(1, 4, 5, 'Samin', 'Alice in Wonderland', 340),
+(2, 4, 5, 'Samin', 'Alice in Wonderland', 340),
+(3, 4, 4, 'Samin', 'Adventures of Huckleberry Finn', 100),
+(4, 4, 3, 'Samin', 'The Art of War', 215),
+(5, 4, 5, 'Samin', 'Alice in Wonderland', 340);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `book_purchase`
 --
 
@@ -62,7 +88,8 @@ CREATE TABLE `book_purchase` (
   `user_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `user_name` varchar(255) NOT NULL,
-  `product_name` varchar(255) NOT NULL
+  `product_name` varchar(255) NOT NULL,
+  `price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -124,6 +151,14 @@ ALTER TABLE `books`
   ADD KEY `category_id` (`category_id`);
 
 --
+-- Indexes for table `book_cart`
+--
+ALTER TABLE `book_cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
 -- Indexes for table `book_purchase`
 --
 ALTER TABLE `book_purchase`
@@ -180,6 +215,13 @@ ALTER TABLE `users`
 --
 ALTER TABLE `books`
   ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+
+--
+-- Constraints for table `book_cart`
+--
+ALTER TABLE `book_cart`
+  ADD CONSTRAINT `book_cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `book_cart_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `books` (`id`);
 
 --
 -- Constraints for table `book_purchase`
