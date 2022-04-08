cdd2fa5fdbdb0bd8e568c090e8ccb8a1d7c1f882
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-04-09 05:59:11 +0600

    Register Validation Added

--- register.php
+++ register.php
@@ -72,30 +72,36 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <h1 class="heading mt-5 h1">Register</h1>
     <div class="register">
         <div class="register-form">
-            <form class="login-form mx-auto" action="register.php" method="post">
+            <form class="login-form mx-auto" action="register.php" id="form-input" method="post">
                 <div class="form-group">
                     <label for="name">Name</label>
                     <input type="text" class="form-control" id="name" placeholder="Enter name" name="name" required>
+                    <p id="alertName" class="text-danger font-weight-bold form-text"></p>
                 </div>
                 <div class="form-group">
                     <label for="username">Username</label>
                     <input type="text" class="form-control" id="username" placeholder="Enter username" name="username" required>
+                    <p id="alertUsername" class="text-danger font-weight-bold form-text"></p>
                 </div>
                 <div class="form-group">
                   <label for="email">Email address</label>
                   <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
+                  <p id="alertEmail" class="text-danger font-weight-bold form-text"></p>
                 </div>
                 <div class="form-group">
                   <label for="password">Password</label>
                   <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
+                  <p id="alertPassword" class="text-danger font-weight-bold form-text"></p>
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Confirm Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirm Password" name="cpassword" required>
+                    <input type="password" class="form-control" id="confirm-password" placeholder="Confirm Password" name="cpassword" required>
+                    <p id="alertConfirmPassword" class="text-danger font-weight-bold form-text"></p>
                 </div>
                 <div class="form-group">
-                    <label for="location">Location</label>
+                    <label for="location">Delivery Address</label>
                     <input type="text" class="form-control" id="location" placeholder="Enter your location" name="location" required>
+                    <p id="alertLocation" class="text-danger font-weight-bold form-text"></p>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
               </form>
@@ -112,7 +118,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="js/app.js"></script>
+    <script src="js/register.js"></script>
     
 </body>
 </html>
