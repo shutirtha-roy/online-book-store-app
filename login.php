2de4d254d82469f117f9aafaa1de13af3eb4c8a5
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-04-09 05:08:09 +0600

    Login Validation In Progress

--- login.php
+++ login.php
@@ -60,15 +60,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <h1 class="heading mt-5 h1">Login</h1>
    
-    <div class="login">
+    <div class="login" id="login-form-input">
         <form class="login-form mx-auto m-2 p-5" action="login.php" method="post">
             <div class="form-group">
               <label for="exampleInputEmail1">Email address</label>
               <input type="email" class="form-control" id="email" name="email"  placeholder="Enter email" required>
+              <p id="alertEmail" class="text-danger font-weight-bold form-text"></p>
             </div>
             <div class="form-group">
               <label for="exampleInputPassword1">Password</label>
               <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+              <p id="alertPassword" class="text-danger font-weight-bold form-text"></p>
             </div>
             <button type="submit" class="btn btn-primary">Login</button>
           </form>
@@ -80,7 +82,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="js/app.js"></script>
+    <script type="text/javascript" src="js/login.js"></script>
     
 </body>
 </html>
